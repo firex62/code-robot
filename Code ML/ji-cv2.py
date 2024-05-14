f40cd1465be46cfbf3b2491b3bea7af8eea66f1b
Muhammad Firman Yassin <firman020602@gmail.com>
2024-05-14 23:33:17 +0800

    Bounding box center

--- Code ML/ji-cv2.py
+++ Code ML/ji-cv2.py
@@ -2,7 +2,8 @@ import cv2
 import jetson.inference
 import jetson.utils
 
-net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold = 0.5)
+#net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold = 0.5)
+net = jetson.inference.detectNet(argv=['--input-blob=input_0', '--model=/home/kukulai/Downloads/model/detect-sampah3/ssd-mobilenet.onnx', '--output-cvg=scores', '--output-bbox=boxes', '--labels=/home/kukulai/Downloads/model/detect-sampah3/labels.txt', '--overlay=lines,labels,conf'],threshold = 0.5)
 camera = jetson.utils.gstCamera(640, 360, "/dev/video0")
 display = jetson.utils.glDisplay()
 
@@ -13,6 +14,10 @@ while display.IsOpen():
     detections = net.Detect(img, width, height)
     display.RenderOnce(img, width, height)
 
-    cv2.imshow('hello', img)
+    for det in detections:
+        print(det.ClassID)
+        print(det.Center)
+
+    # cv2.imshow('hello', img)
 
 cv2.destroyAllWindows()
